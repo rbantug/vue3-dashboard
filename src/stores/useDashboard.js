8c0aeb0d0eecb90fe46621e4a61ce837c11c2bf3
rbantug <ronbantug22@gmail.com>
2023-07-15 16:45:43 +0800

    updated useDashboard.js

--- src/stores/useDashboard.js
+++ src/stores/useDashboard.js
@@ -4,7 +4,6 @@ export const useDashboardStore = defineStore('dashboard', {
     state: () => ({
         recentTransactionData,
         pendingNotifyIdDeleteInvoice,
-        clientList,
         msgNotification,
         notifications,
         notifyPingIsVisible,
